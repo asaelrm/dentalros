fdb7a3d17e7eb77b184fabfff7bbea37fd88a8e9
asaelrm <asaelrm0233@gmail.com>
2026-09-12 23:34:31 +0000

    Vaciar repositorio para nuevo proyecto

--- Abrir_Carpeta_Proyecto.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-explorer "%~dp0"
-exit
--- Iniciar_Sistema.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Acceso directo para iniciar el Sistema Odontológico en el navegador predeterminado
-title Sistema Odontologico
-echo Iniciando Sistema de Gestion Odontologica...
-start "" "%~dp0index.html"
-exit
